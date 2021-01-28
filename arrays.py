9436689b3007a5a4ae3f920b7744aa5ef56eb5aa
AumkarMali <78087877+AumkarMali@users.noreply.github.com>
2021-01-28 09:26:11 -0500

    Add files via upload

---
+++ arrays.py
@@ -0,0 +1,131 @@
+
+burns = [
+    "You’re the reason God created the middle finger.",
+    "You’re a grey sprinkle on a rainbow cupcake.",
+    "If your brain was dynamite, there wouldn’t be enough to blow your hat off.",
+    "You are more disappointing than an unsalted pretzel.",
+    "Light travels faster than sound which is why you seemed bright until you spoke.",
+    "We were happily married for one month, but unfortunately we’ve been married for 10 years.",
+    "Your so annoying, you made your Happy Meal cry.",
+    "You have so many gaps in your teeth it looks like your tongue is in jail.",
+    "Your secrets are always safe with me. I never even listen when you tell me them.",
+    "I’ll never forget the first time we met. But I’ll keep trying.",
+    "I forgot the world revolves around you. My apologies, how silly of me.",
+    "I only take you everywhere I go just so I don’t have to kiss you goodbye.",
+    "Hold still. I’m trying to imagine you with personality.",
+    "Our kid must have gotten his brain from you! I still have mine.",
+    "Your face makes onions cry.",
+    "The only way my husband would ever get hurt during an activity is if the TV exploded.",
+    "You look so pretty. Not at all gross, today.",
+    "911, emergency we have spotted shrek, ohh wait never mind it was you.",
+    "If you’re going to be two-faced, at least make one of them pretty.",
+    "You are like a cloud. When you disappear it’s a beautiful day.",
+    "I’d rather treat my baby’s diaper rash than have lunch with you.",
+    "Even with with Mcafee my CPU crumbles in the virus that is your creativity.",
+    "I may love to shop but I will never buy your bull.",
+    "I love what you’ve done with your hair. How do you get it to come out of your nostrils like that?"
+            ]
+
+hug_links = [
+    "https://tenor.com/view/warm-hug-gif-10592083",
+    "https://tenor.com/view/cat-hug-dog-annoyed-puppy-gif-15579685",
+    "https://tenor.com/view/cat-hug-back-hug-notice-me-attention-to-me-gif-14227401",
+    "https://tenor.com/view/milk-and-mocha-hugs-bear-couple-love-cute-gif-12498539",
+    "https://tenor.com/view/hug-anime-gif-7552075",
+    "https://tenor.com/view/hug-anime-love-gif-7324587",
+    "https://tenor.com/view/cuddle-hug-comfort-cute-sweet-gif-16300139",
+    "https://tenor.com/view/anime-cheeks-hugs-gif-14106856",
+    "https://tenor.com/view/hug-cry-sad-anime-tackle-gif-5634582",
+    "https://tenor.com/view/anime-hug-sweet-love-gif-14246498",
+    "https://tenor.com/view/anime-hug-hug-hugs-anime-girl-anime-girl-hug-gif-16787485",
+    "https://tenor.com/view/sakura-quest-anime-animes-hug-hugging-gif-14721541"
+    "https://tenor.com/view/running-hug-embrace-imiss-you-good-to-see-you-again-gif-15965620",
+    "https://tenor.com/view/snoopy-hug-love-hugs-woodstuck-gif-11557800",
+    "https://tenor.com/view/baby-pooh-disney-winnie-the-pooh-piglet-hearts-gif-11070433",
+    "https://tenor.com/view/my-life-ilove-my-best-friend-bff-hugs-lilo-and-stitch-gif-15915447"
+            ]
+
+
+bad_words = ["Fuck", "shit", "nig", "fag", "bitc", "slut", "cum", "kum", "bal", "cun"]
+
+no = ["trash", "die", "garb", "ass", "poo", "pee", "suck", "bad", "terrible", "horrid", "horrible", "horrendous", "broken", "broke", "dum", "stupid", "gay", "ugly", "hideous", "moron", "idiot", "bum", "butt", "Fuck", "shit", "nig", "fag", "bitc", "slut", "cum", "kum", "bal", "cun", "no one care", "stinky", "smelly"]
+
+compliments = ["awesome", "great", "good", "well made", "cute", "beaut", "gorg", "smart", "hot", "sweet", "like", "love", "cool"]
+compliment_back = ["Your positivity is infectious.",
+"You should be so proud of yourself.",
+"You’re amazing!",
+"You’re a true gift to the people in your life.",
+"You’re an incredible friend.",
+"I really appreciate everything that you do.",
+"Your passion always motivates me.",
+"Your smile makes me smile.",
+"Thank you for being such a great person.",
+"The way you carry yourself is truly admirable.",
+"You are such a good listener.",
+"Thanks for being you!",
+"You set a great example for everyone around you."
+                   ]
+
+dont_say_that = ["That ain't nice...", "refrain from saying those things...", "Close that big mouth of yours...", "Although it isn't a swear its definetly offending someone..."]
+moods = ["Pretty cheerful", "quite cheerful", "Joyous", "Voracious", "Happy", " A bit giddy", "Glad im alive"]
+not_here = ["Not in my database.", "Not found in database.", "My program doesnt allow those records to be sent...", "Could not be found...", "Information is not found"]
+
+
+cat_pics = ["https://tenor.com/view/cat-waiting-gif-5701246",
+            "https://tenor.com/view/peachcat-peach-and-goma-eat-noodle-bowl-ramen-gif-14819436",
+            "https://tenor.com/view/kitten-cats-cutie-cute-morning-gif-13147092",
+            "https://tenor.com/view/cat-hugging-cats-cuddle-gif-8123182",
+            "https://tenor.com/view/love-kittens-cute-cats-cute-kittens-kitten-cuddles-gif-13033361",
+            "https://tenor.com/view/hug-peachcat-cat-cute-gif-13985247",
+            "https://tenor.com/view/luv-you-cute-kitten-cat-cats-kittens-gif-13071893"]
+
+
+en = ["Hello there.", "Greetings", "A apple a day keeps the doctor away", "English is spoken by 1.5 billion people", "English is the language of engineering."]
+
+fr = ["Battre le fer pendant qu'il est chaud.",
+"Ce n'est pas la mer à boire",
+"La nuit porte conseil",
+"Vouloir, c'est pouvoir",
+"Impossible n'est pas français",
+"Il ne faut rien laisser au hasard",
+"L'habit ne fait pas le moine",
+"Mieux vaut tard que jamais",
+      "Bonjour!"]
+
+span = ['Camarón que se duerme se lo lleva la corriente. ',
+'A lo hecho, pecho. ',
+'En boca cerrada no entran moscas. ',
+'Cría cuervos y te sacarán los ojos. ',
+'A mal tiempo, buena cara. ',
+'El que quiera pescado que se moje el culo. ',
+'Más vale ser cabeza de ratón que cola de león. ',
+'Moro viejo nunca será buen cristiano. ']
+
+jap = ["自業自得", "十人十色", "起死回生", "我田引水", "悪因悪果", "見ぬが花", "弱肉強食"]
+
+brit = ["Alright",
+"I’m knackered",
+"Cheeky",
+"Bloody brilliant",
+"I’m pissed",
+"Hey Mate",
+"Rubbish",
+"Cockney.",
+"Blimey",
+"Bollocks"]
+
+amer = ["Howdy", "Pass em big macs", "Sweet home alabama", "Piece of cake.",
+"Scoot over.",
+"Put lipstick on a pig.",
+"Break a leg.",
+"Knock on wood.",
+"Not a big fan.",
+"It's not rocket science.",
+"Break a bill."]
+
+canto = ["多謝", "你好", "早晨", "晚安", "你好嗎", "午安"]
+mand = ["你好", "谢谢", "不用谢", "你好吗", "好的", "请问"]
+
+kor = ["고생 끝에 낙이 온다", "개천에서 용 난다", "개똥도 약에 쓰려면 없다", "공자 앞에서 문자 쓴다", "옷이 날개다", "원숭이도 나무에서 떨어진다", "매도 먼저 맞는 게 낫다"]
+
+anime = ["Naruto", "Attack on Titan", "One piece", "Bleach", "Seven deadly Sins", "One punch man"]
